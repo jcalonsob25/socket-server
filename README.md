7dd347845435efe5e230972d0059c70a715cfec1
Juan Alonso <abjo151620@upemor.edu.mx>
2020-04-20 14:40:42 -0500

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socket-Server
+
+Reconstruir módulos de Node
+
+npm install
+
+Generar el DIST
+
+
+tsc -w
+
+
+Escuchar los cambios
+
+nodemon dist/
---
+++ routes/router.ts
@@ -0,0 +1,39 @@
+import {Router, Request, Response} from 'express';
+
+const router = Router();
+
+
+router.get('/mensajes', (req: Request, res: Response) =>{
+    res.json({
+        ok: true,
+        mensaje: 'Todo está bien'
+    })
+})
+
+router.post('/mensajes', (req: Request, res: Response) =>{
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    res.json({
+        ok: true,
+        mensaje: 'Todo está bien  POST',
+        cuerpo,
+        de
+    })
+})
+
+router.post('/mensajes/:id', (req: Request, res: Response) =>{
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+    res.json({
+        ok: true,
+        mensaje: 'Todo está bien  POST',
+        cuerpo,
+        de,
+        id
+    })
+})
+
+
+
+export default router;
